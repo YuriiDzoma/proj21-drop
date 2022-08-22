402e5bac40160a4324a7996dbc83f699f3c48fdd
Yurii Dzoma <yuriidzoma@gmail.com>
2022-08-22 18:11:12 +0300

    remake

--- js/script.js
+++ js/script.js
@@ -7,237 +7,235 @@ const allClients = [
     {Name:'Koki', Surname: 'Doki', Strength: 6, Dexterity: 4, Stamina:7},
 ]
 
-class ClientGym {
-    static type = 'Clients of GYM';
+const allRooms = [
+    {Exercise:'Dumbbells', Skil: 'Strength'},
+    {Exercise:'Boxing', Skil: 'Dexterity'},
+    {Exercise:'Run', Skil: 'Stamina'},
+]
+
+class RoomOfGym {
+    constructor (Exercise, Skil) {
+        this.Exercise = Exercise;
+        this.Skil = Skil;
+    } 
+}
+
+class ClientOfGym {
     constructor (Name, Surname, Strength, Dexterity, Stamina) {
         this.Name = Name;
         this.Surname = Surname;
         this.Strength = Strength;
         this.Dexterity = Dexterity;
         this.Stamina = Stamina;
-    } 
+    }
+
+    
 }
 
-const clientsArr = [];
+const clientArr = [];
 
 const personalList = document.querySelector('.personal-list')
-allClients.map((client) => {
 
-    client = new ClientGym(client.Name, client.Surname, client.Strength, client.Dexterity, client.Stamina);
-    clientsArr.push(client);
+allClients.map((client) => {
+    client = new ClientOfGym(client.Name, client.Surname, client.Strength, client.Dexterity, client.Stamina);
+    clientArr.push(client);
 })
-function refreshClient() {
-clientsArr.map((client) => {
-    personalList.insertAdjacentHTML(
-        'afterend',
-        `<div id="${client.Name.toLowerCase()}${client.Surname.toLowerCase()}" class="clients">
-            <div class="client-items">
-                <div class="drag client-items__logo">
-                    <span>${client.Name.substring(0, 1)}${client.Surname.substring(0, 1)}</span>
+
+function conclusionClient() {
+    clientArr.map((client) => {
+        personalList.insertAdjacentHTML(
+            'beforeend',
+            `<div id="${client.Name.toLowerCase()}${client.Surname.toLowerCase()}" class="client"  draggable="true">
+                <div class="client-items">
+                    <div class="drag client-items__logo">
+                        <span>${client.Name.substring(0, 1)}${client.Surname.substring(0, 1)}</span>
+                    </div>
+                    <span class="drag client-items__name">${client.Name} ${client.Surname}</span>
+                    <div  class="client-items__char">
+                        <button class="butt-char"></button>
+                    </div>
                 </div>
-                <span class="drag client-items__name">${client.Name} ${client.Surname}</span>
-                <div  class="client-items__char">
-                    <button class="butt-char"></button>
+                <div class="person-char">
+                    <p class="strength">${'Strength:'} ${client.Strength}</p>
+                    <p class="dexterity">${'Dexterity:'} ${client.Dexterity}</p>
+                    <p class="stamina">${'Stamina:'} ${client.Stamina}</p>  
                 </div>
-            </div>
-            <div class="person-char">
-                <p class="strength">${'Strength:'} ${client.Strength}</p>
-                <p class="dexterity">${'Dexterity:'} ${client.Dexterity}</p>
-                <p class="stamina">${'Stamina:'} ${client.Stamina}</p>  
-            </div>
-            <div class="upgrade-char">
-                <p class="up-strength">${'Upgrade Strength:'} ${client.Strength} ${'/ 15 per 1 minute'}</p>
-                <p class="up-dexterity">${'Upgrade Dexterity:'} ${client.Dexterity} ${'/ 15 per 1 minute'}</p>
-                <p class="up-stamina">${'Upgrade Stamina:'} ${client.Stamina} ${'/ 15 per 1 minute'}</p>
-            </div>
-        </div>`
+                <div class="upgrade-char">
+                    <p class="up-strength">${'Upgrade Strength:'} ${client.Strength} ${'/ 15 per 1 minute'}</p>
+                    <p class="up-dexterity">${'Upgrade Dexterity:'} ${client.Dexterity} ${'/ 15 per 1 minute'}</p>
+                    <p class="up-stamina">${'Upgrade Stamina:'} ${client.Stamina} ${'/ 15 per 1 minute'}</p>
+                </div>
+            </div>`
         );
-})
+    })
 }
-refreshClient()
+conclusionClient();
+
+const roomArr = [];
+
+const gymComplex = document.querySelector('.right-container')
+
+allRooms.map((room) => {
+    room = new RoomOfGym(room.Exercise, room.Skil);
+    roomArr.push(room);
+})
+
+function conclusionRoom() {
+    roomArr.map((room) => {
+        gymComplex.insertAdjacentHTML(
+            'beforeend',
+            `<div class="train-room" id="${room.Exercise.toLowerCase()}">
+                <div class="train-header">
+                    <p class="train-header__name">${room.Skil}</p>
+                    <p class="train-header__counter"></p>
+                </div>
+                <div class="guest-container">
+                    <div class="client-position-left">
+                        <div class="client-position-left__add"></div>
+                    </div>
+                    <div class="client-position-right">
+                        <div class="client-position-left__add"></div>
+                    </div>
+                </div>
+            </div>`
+        );
+    });
+}
+conclusionRoom();
+
+const clients = document.querySelectorAll('.client');
+const rooms = document.querySelectorAll('.train-room');
+
+
 
 const personalCounter = document.querySelector('.personal-counter');
-personalCounter.innerHTML = ' ' + allClients.length;
-
-const area = document.querySelector('.wrapper');
-const buttonExpand = document.querySelectorAll('.butt-char')
-
-
-let addStrength;
-let addDexterity;
-let addStamina;
-
-
-function upgradeStrength(obj,strengthChar,strengthUp) {
-    addStrength = setInterval(function() {
-    obj.Strength++;
-    strengthChar.innerHTML = `${'Strength:'} ${obj.Strength}`;
-    strengthUp.innerHTML = `${'Upgrade Strength:'} ${obj.Strength} ${'/ 15 per 1 minute'}`;
-    }, 4000);
+const homeList = document.querySelector('.personal-list');
+function getCounterHome() {
+    personalCounter.innerHTML = ' ' + homeList.childNodes.length;
 }
+getCounterHome() 
 
-function upgradeDexterity(obj,dexterityChar,dexterityUp) {
-    addDexterity = setInterval(function() {
-    obj.Dexterity++;
-    dexterityChar.innerHTML = `${'Dexterity:'} ${obj.Dexterity}`;
-    dexterityUp.innerHTML = `${'Upgrade Dexterity:'} ${obj.Dexterity} ${'/ 15 per 1 minute'}`;
-    }, 4000);
+
+
+const guestCounter = document.querySelectorAll('.train-header__counter');
+
+function getCounterGym() {
+    guestCounter.forEach(counter => {
+        const parent = counter.closest('.train-room');
+        counter.innerHTML = parent.childNodes.length - 5 + `/2`;
+    })
 }
-
-function upgradeStamina(obj,staminaChar,staminaUp) {
-    addStamina = setInterval(function() {
-    obj.Stamina++;
-    staminaChar.innerHTML = `${'Stamina:'} ${obj.Stamina}`;
-    staminaUp.innerHTML = `${'Upgrade Stamina:'} ${obj.Stamina} ${'/ 15 per 1 minute'}`;
-    }, 4000);
-}
-
-function stopUpgradeStrength(obj) {
-    clearInterval(addStrength);
-};
-
-function stopUpgradeDexterity(obj) {
-    clearInterval(addDexterity);
-};
-
-function stopUpgradeStamina(obj) {
-    clearInterval(addStamina);
-};
-
-document.querySelectorAll('.drag').forEach(client => {
-    client.addEventListener("mousedown", function (raisedEvent) {
-        const parent = client.closest('.clients');
-        let coordsItemX = raisedEvent.clientX - parent.getBoundingClientRect().left;
-        let coordsItemY = raisedEvent.clientY - parent.getBoundingClientRect().top;
-    
-        let gragItemSizes = {
-            width: parent.offsetWidth,
-            height: parent.offsetHeight
-        }
-        let gragFieldSizes = {
-            left: area.getBoundingClientRect().left + scrollX,
-            top: area.getBoundingClientRect().top + scrollY,
-            right: area.getBoundingClientRect().left + scrollX + area.offsetWidth,
-            bottom: area.getBoundingClientRect().top + scrollY + area.offsetHeight
-        }
-        
-    
-        parent.style.position = 'absolute';
-        parent.style.zIndex = 1000;
-        document.body.append(parent);
-    
-        moveItem(raisedEvent.pageX, raisedEvent.pageY);
-    
-        function moveItem(pageX, pageY) {
-            let currentX = pageX - coordsItemX;
-            let currentY = pageY - coordsItemY;
-    
-            if (
-                currentX + gragItemSizes.width <= gragFieldSizes.right &&
-                currentX >= gragFieldSizes.left
-            ) {
-                parent.style.left = `${currentX}px`;
-            } else {
-                if (currentX + gragItemSizes.width > gragFieldSizes.right) {
-                    parent.style.left = `${gragFieldSizes.right - gragItemSizes.width}px`;
-                }
-                if (currentX < gragFieldSizes.left) {
-                    parent.style.left = `${gragFieldSizes.left}px`;
-                }
-            }
-            if (
-                currentY + gragItemSizes.height <= gragFieldSizes.bottom &&
-                currentY >= gragFieldSizes.top
-            ) {
-                parent.style.top = `${currentY}px`;
-            } else {
-                if (currentY + gragItemSizes.height > gragFieldSizes.bottom) {
-                    parent.style.top = `${gragFieldSizes.bottom - gragItemSizes.height}px`;
-                }
-                if (currentY < gragFieldSizes.top) {
-                    parent.style.top = `${gragFieldSizes.top}px`;
-                }
-            }
-        }
-    
-        
-
-        function onDragItem(event) {
-            moveItem(event.pageX, event.pageY);
-            
-            parent.hidden = true;
-            let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-            parent.hidden = false;
-            if (!elemBelow) return;
-
-            
-        }
-        document.addEventListener('mousemove', onDragItem);
-    
-        document.addEventListener("mouseup", function (dropElement) {
-            
-            parent.hidden = true;
-            let elemBelow = document.elementFromPoint(dropElement.clientX, dropElement.clientY);
-            parent.hidden = false;
-            if (!elemBelow) return;
-            
-            let trainingStrength = elemBelow.closest('.trainig-1');
-            let trainingDexterity = elemBelow.closest('.trainig-2');
-            let trainingStamina = elemBelow.closest('.trainig-3');
-
-            let obj = clientsArr.find((item) => {
-                return item.Name.toLowerCase() + item.Surname.toLowerCase() == parent.id;
-            });
-            
-            let strengthChar = parent.querySelector('.strength');
-            let dexterityChar = parent.querySelector('.dexterity');
-            let staminaChar = parent.querySelector('.stamina');
-
-            let strengthUp = parent.querySelector('.up-strength');
-            let dexterityUp = parent.querySelector('.up-dexterity');
-            let staminaUp = parent.querySelector('.up-stamina');
-
-            if (trainingStrength) {
-                parent.classList.add('strength');
-                upgradeStrength(obj,strengthChar,strengthUp);
-            } else {
-                stopUpgradeStrength();
-                parent.classList.remove('strength');
-                parent.classList.remove('expand');
-            }
-
-            if (trainingDexterity) {
-                parent.classList.add('dexterity');
-                upgradeDexterity(obj,dexterityChar,dexterityUp);
-            } else {
-                stopUpgradeDexterity();
-                parent.classList.remove('dexterity');
-                parent.classList.remove('expand');
-            }
-
-            if (trainingStamina) {
-                parent.classList.add('stamina');
-                upgradeStamina(obj,staminaChar,staminaUp);
-            } else {
-                stopUpgradeStamina();
-                parent.classList.remove('stamina');
-                parent.classList.remove('expand');
-            }
-            document.removeEventListener('mousemove', onDragItem);
+getCounterGym()
 
 
-        }, { "once": true });
-    });
-    parent.addEventListener("dragstart", function (start) {
-        start.preventDefault();
-    }); 
-})
+const buttonExpand = document.querySelectorAll('.butt-char');
 
 
 document.querySelectorAll('.butt-char').forEach(but => {
     but.addEventListener('click', function() {
-        but.closest('.clients').classList.toggle('expand');
+        but.closest('.client').classList.toggle('expand');
     })
 })
 
+const dragOver = function (event) {
+    event.preventDefault();
+}
 
+const dragEnter = function () {
+    if (this.childNodes[5]) {
+        this.childNodes[5].classList.add('lock');
+    }
+}
+
+
+ondrop = ()=> {
+    clients.forEach((client) => {
+        client.classList.remove('lock');
+    }) 
+}
+
+rooms.forEach((room) => {
+    room.addEventListener('dragover', dragOver);
+    room.addEventListener('dragenter', dragEnter);
+})
+
+const HomeContainer = document.querySelector('.box-items');
+HomeContainer.addEventListener('dragover', dragOver);
+
+function checkTheRoom(client) {
+    rooms.forEach((room) => {
+        const roomPosLeft = room.getBoundingClientRect().left;
+        const roomPosTop = room.getBoundingClientRect().top;
+        const roomWidth = room.clientWidth;
+        const roomHeight = room.clientHeight;
+  
+        if (client.clientX > roomPosLeft &&
+            client.clientX < roomPosLeft + roomWidth &&
+            client.clientY > roomPosTop &&
+            client.clientY < roomHeight + roomPosTop)
+        {
+            if (room.childNodes.length <= 6) {
+                room.append(client.target);
+                client.target.className = 'client';
+                client.target.classList.add(room.id);
+                getCounterGym();
+                getCounterHome();
+            }
+        }
+    })
+}
+
+const home = document.querySelector('.box-items');
+
+function checkTheHome(client) {       
+    const homePosLeft = home.getBoundingClientRect().left;
+    const homePosTop = home.getBoundingClientRect().top;
+    const homeWidth = home.clientWidth;
+    const homeHeight = home.clientHeight;
+
+    if (client.clientX > homePosLeft &&
+        client.clientX < homePosLeft + homeWidth &&
+        client.clientY > homePosTop &&
+        client.clientY < homeHeight + homePosTop)
+    {
+        homeList.append(client.target);
+        client.target.className = 'client';
+        getCounterGym();
+        getCounterHome();
+    }
+}
+
+clients.forEach((person) => {
+    person.addEventListener('dragend', function (client) {
+      checkTheRoom(client);
+      checkTheHome(client);
+    })
+})
+
+rooms.forEach((room) => {
+    room.addEventListener('dragenter', ()=> {
+        if (room.childNodes.length == 5) {
+            room.querySelector('.client-position-left').classList.add('visible');
+        }
+        if (room.childNodes.length == 6) {
+            room.querySelector('.client-position-right').classList.add('visible');
+        }
+    })
+})
+
+// ondragend = ()=> {
+//     rooms.forEach((room) => {
+//         if (room.childNodes.length == 6) {
+//             room.querySelector('.client-position-right').classList.add('visible');
+//         } else {room.querySelector('.client-position-right').classList.remove('visible')}
+//     })
+// }
+ 
+
+
+rooms.forEach((room) => {
+    room.addEventListener('dragleave', ()=> {
+        room.querySelector('.client-position-left').classList.remove('visible');
+        room.querySelector('.client-position-right').classList.remove('visible');
+    })
+})
